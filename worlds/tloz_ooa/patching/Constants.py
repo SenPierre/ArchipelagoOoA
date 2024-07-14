19fefe63c160c7b208caa0aed59ad99a53a1c55d
SenPierre <seneclauze.pierre@gmail.com>
2024-07-14 13:26:13 +0200

    Reassign the EOB

--- worlds/tloz_ooa/patching/Constants.py
+++ worlds/tloz_ooa/patching/Constants.py
@@ -1,68 +1,68 @@
 EOB_ADDR = [
-    0x3ef9,  # 00
-    0x7fc5,  # 01
-    0x7e96,  # 02
-    0x7ebe,  # 03
-    0x7ee3,  # 04
-    0x7d9d,  # 05
-    0x7a32,  # 06 - 128 bytes reserved for sprite expansion w/ web patcher (Piapiou : True there too ?)
-    0x7de0,  # 07
-    0x7f5c,  # 08
-    0x7def,  # 09
-    0x7e08,  # 0a
-    0x7fa9,  # 0b
-    0x7f94,  # 0c
-    0x7eaa,  # 0d
-    0x7f88,  # 0e
-    0x7F91,  # 09
-    0x7ef5,  # 10
-    0x7f74,  # 11
-    0x7e90,  # 12
-    0x7ef0,  # 13
-    0x7abe,  # 14 - A bit of space here
-    0x7bfc,  # 15
-    0x7e03,  # 16
-    0x7f2b,  # 17
-    0x7e6d,  # 18
-    0x7fd0,  # 19
-    0x7ec1,  # 1a
-    0x7ee1,  # 1b
-    0x8000,  # 1c
-    0x8000,  # 1d
-    0x8000,  # 1e
-    0x8000,  # 1f
-    0x8000,  # 20
-    0x8000,  # 21
-    0x8000,  # 22
-    0x8000,  # 23
-    0x8000,  # 24
-    0x8000,  # 25
-    0x8000,  # 26
-    0x8000,  # 27
-    0x8000,  # 28
-    0x8000,  # 29
-    0x8000,  # 2a
-    0x8000,  # 2b
-    0x8000,  # 2c
-    0x8000,  # 2d
-    0x8000,  # 2e
-    0x8000,  # 2f
-    0x8000,  # 30
-    0x8000,  # 31
-    0x8000,  # 32
-    0x8000,  # 33
-    0x8000,  # 34
-    0x8000,  # 35
-    0x8000,  # 36
-    0x8000,  # 37
-    0x6af0,  # 38 - Lot of space here
-    0x7f5e,  # 39
-    0x8000,  # 3a
-    0x7f68,  # 3b
-    0x7d5a,  # 3c
-    0x7fa3,  # 3d
-    0x8000,  # 3e
-    0x7d0b   # 3f 
+  0x3ef8, # 00
+  0x7f23, # 01 - garbage data here
+  0x7de7, # 02 - garbage data here
+  0x7e54, # 03 - garbage data here
+  0x7ee2, # 04
+  0x7d9d, # 05
+  0x7a73, # 06 - garbage data here - 128 bytes reserved for sprite expansion w/ web patcher
+  0x7caa, # 07 - garbage data here
+  0x7f5c, # 08
+  0x7def, # 09
+  0x7e08, # 0a
+  0x7fa8, # 0b
+  0x7f94, # 0c
+  0x7eaa, # 0d
+  0x7f88, # 0e
+  0x7f90, # 0f
+  0x7ef4, # 10
+  0x7f73, # 11
+  0x7e8f, # 12
+  0x7ef0, # 13
+  0x7acd, # 14
+  0x7bfb, # 15
+  0x7e03, # 16
+  0x6ee3, # 17 - garbage data here (lots of space here)
+  0x799e, # 18 - garbage data here
+  0x7fdf, # 19
+  0x7ed0, # 1a
+  0x7ee0, # 1b
+  0x7dc0, # 1c - garbage data here
+  0x8000, # 1d
+  0x8000, # 1e
+  0x8000, # 1f
+  0x8000, # 20
+  0x8000, # 21
+  0x8000, # 22
+  0x8000, # 23
+  0x8000, # 24
+  0x8000, # 25
+  0x8000, # 26
+  0x8000, # 27
+  0x8000, # 28
+  0x8000, # 29
+  0x8000, # 2a
+  0x8000, # 2b
+  0x8000, # 2c
+  0x8000, # 2d
+  0x8000, # 2e
+  0x8000, # 2f
+  0x8000, # 30
+  0x8000, # 31
+  0x8000, # 32
+  0x8000, # 33
+  0x8000, # 34
+  0x8000, # 35
+  0x8000, # 36
+  0x8000, # 37
+  0x6afb, # 38 - lots of space here
+  0x8000, # 39
+  0x8000, # 3a
+  0x8000, # 3b
+  0x8000, # 3c
+  0x8000, # 3d
+  0x8000, # 3e
+  0x7ca7  # 3f - garbage data here
 ]
 
 DEFINES = {
